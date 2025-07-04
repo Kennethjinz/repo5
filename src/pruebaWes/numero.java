0c4b47af056666dff8f435dd1edc9e5cd26bf90d
Wesley Ruiz <wruizm@est.utn.ac.cr>
2025-07-04 10:24:59 -0600

    Se crea un enum
    Se crea un enum para estado

--- src/pruebaWes/numero.java
+++ src/pruebaWes/numero.java
@@ -9,5 +9,21 @@ package pruebaWes;
  * @author Student
  */
 public enum numero {
-    
+  OCUPADO("Ocupado"),
+  LIBRE("Libre");
+  
+  private final String estado;
+
+    public String getEstado() {
+        return estado;
+    }
+
+    private numero(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "numero{" + "ordinal=" + ordinal() + ", name=" + name() + ", estado=" + estado + '}';
+    }
 }
